17e4abd970e659e5bf162c71d7b5de33053a0332
usku8 <usku8.siticom@gmail.com>
2020-06-11 06:41:26 +0800

    first commit

---
+++ app/Http/Controllers/AjaxController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AjaxController extends Controller
+{
+	public function ajaxRequest()
+	{
+		return view('ajaxRequest');
+	}
+	
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function ajaxRequestPost(Request $request)
+    {
+    	$input = $request->all();
+    	\Log::info($input);
+    	
+    	return response()->json(['success'=>'Got Simple Ajax Request.']);
+    }
+}
